3ccaeb0ad8a3ba8f79258d8908ed6bab774a3a41
LiamLime <contact@liamlime.com>
2021-09-23 22:03:25 +0100

    Moves ifThen, ifElse and ifBlankElse tests into their alphabetical position

--- tests/MapTest.php
+++ tests/MapTest.php
@@ -1141,6 +1141,36 @@ Array
 	}
 
 
+    public function testIfThen()
+    {
+        Map::from( ['a' => 1, 'b' => 0] )->if(
+            fn(Map $map) => $map->has('a'),
+            function(Map $_) { $this->assertTrue(true); },
+            function(Map $_) { $this->assertTrue(false);}
+        );
+    }
+
+    
+    public function testIfElse()
+    {
+        Map::from( ['a' => 1, 'b' => 0] )->if(
+            fn(Map $map) => $map->has('c'),
+            function(Map $_) { $this->assertTrue(false); },
+            function(Map $_) { $this->assertTrue(true);}
+        );
+    }
+
+
+    public function testIfBlankElse()
+    {
+        Map::from( ['a' => 1, 'b' => 0] )->if(
+            fn(Map $map) => $map->has('c'),
+            function(Map $_) { $this->assertTrue(false); }
+        );
+        $this->assertTrue(true);
+    }
+
+
 	public function testIn()
 	{
 		$this->assertTrue( Map::from( ['a', 'b'] )->in( 'a' ) );
@@ -2658,33 +2688,6 @@ Array
 		];
 
 		$this->assertEquals( $expected, $m->zip( $en, $es )->toArray() );
-	}
-
-	public function testIfThen()
-	{
-		Map::from( ['a' => 1, 'b' => 0] )->if(
-			fn(Map $map) => $map->has('a'),
-			function(Map $_) { $this->assertTrue(true); },
-			function(Map $_) { $this->assertTrue(false);}
-		);
-	}
-
-	public function testIfElse()
-	{
-		Map::from( ['a' => 1, 'b' => 0] )->if(
-			fn(Map $map) => $map->has('c'),
-			function(Map $_) { $this->assertTrue(false); },
-			function(Map $_) { $this->assertTrue(true);}
-		);
-	}
-
-	public function testIfBlankElse()
-	{
-		Map::from( ['a' => 1, 'b' => 0] )->if(
-			fn(Map $map) => $map->has('c'),
-			function(Map $_) { $this->assertTrue(false); }
-		);
-		$this->assertTrue(true);
 	}
 }
 
